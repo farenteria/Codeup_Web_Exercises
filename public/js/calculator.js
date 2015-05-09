0c1e71306a709aa882afbbb50b819bc0b74d5fc7
Fernando Renteria <rentfern@gmail.com>
2015-05-09 09:40:08 -0500

    changed element access to getting by class name instead of id

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -1,3 +1,16 @@
 (function(){
 	"use strict"
+
+	var firstNum = 0;
+	var secondNum = 0;
+	var operator = 0;
+	var firstNumField = document.getElementById("first-num-field");
+	var operField = document.getElementById("oper-field");
+	var secondNumField = document.getElementById("second-num-field");
+	var numbersButtons = document.getElementsByClassName("numbers");
+	var operatorButtons = document.getElementsByClassName("operator");
+
+	function saveNum(){
+
+	}
 })();
