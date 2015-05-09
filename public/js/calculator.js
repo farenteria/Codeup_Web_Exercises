311e51f9b755c917090e5ef94f9f767cd72edfd8
Fernando Renteria <rentfern@gmail.com>
2015-05-09 10:01:47 -0500

    Calculator - clicking on buttons now saves first number and operator

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -1,16 +1,32 @@
 (function(){
 	"use strict"
 
-	var firstNum = 0;
-	var secondNum = 0;
-	var operator = 0;
+	var firstNum = "0";
+	var secondNum = "0";
+	var operator = "0";
 	var firstNumField = document.getElementById("first-num-field");
 	var operField = document.getElementById("oper-field");
 	var secondNumField = document.getElementById("second-num-field");
-	var numbersButtons = document.getElementsByClassName("numbers");
+	var numbersButtons = document.getElementsByClassName("number");
 	var operatorButtons = document.getElementsByClassName("operator");
+	var onFirstNumber = true;
 
-	function saveNum(){
+	for(var i = 0; i < numbersButtons.length; i++){
+		numbersButtons[i].addEventListener("click", function(){
+			if(onFirstNumber){
+				firstNum += this.innerHTML;
+				console.log(firstNum);
+			} else{
+				secondNum += this.innerHTML;
+				console.log(secondNum);
+			}
+		});
+	}
 
+	for(var i = 0; i < operatorButtons.length; i++){
+		operatorButtons[i].addEventListener("click", function(){
+			operator = this.innerHTML;
+			console.log(operator);
+		});
 	}
 })();
