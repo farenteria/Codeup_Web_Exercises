139993bd3050926bebd484f07a231e745988ed15
Fernando Renteria <rentfern@gmail.com>
2015-06-01 20:50:18 -0500

    Weather - Max and min temps are recorded in first box

--- public/js/weather_map.js
+++ public/js/weather_map.js
@@ -3,9 +3,12 @@
 	// Will update HTML page with current weather
 	function updateWeather(data){
 		console.log(data);
+		$("#max-min-1").text(data.main.temp_max + "/" + data.main.temp_min + unescape('%B0'));
 	}
 
 	$.get("http://api.openweathermap.org/data/2.5/weather", {
-		q: "San Antonio, TX"
+		units: "imperial",
+		lat: "29.42", 
+		lon: "-98.49"
 	}).done(updateWeather);
 })();
