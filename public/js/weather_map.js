535b41aeb74e6dc5391f4472283e347134395ec5
Fernando Renteria <rentfern@gmail.com>
2015-06-01 21:01:41 -0500

    Weather - Added icon for weather under temperatures. Changed html and css to accomidate size of icon

--- public/js/weather_map.js
+++ public/js/weather_map.js
@@ -3,7 +3,9 @@
 	// Will update HTML page with current weather
 	function updateWeather(data){
 		console.log(data);
+		var iconUrl = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
 		$("#max-min-1").text(data.main.temp_max + "/" + data.main.temp_min + unescape('%B0'));
+		$("#weather-img-1").attr("src", iconUrl);
 	}
 
 	$.get("http://api.openweathermap.org/data/2.5/weather", {
