3e671130aa0c6c5de36b7d3f91f341892463268e
Fernando Renteria <rentfern@gmail.com>
2015-06-13 15:36:36 -0500

    PHP foreach - constructed loop that prints type of each value in  loop

--- public/foreach.php
+++ public/foreach.php
@@ -1,5 +1,21 @@
 <?php
 	$things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
-	
+	foreach ($things as $item) {
+		if(is_int($item)){
+			echo "Integer";
+		} else if(is_float($item)){
+			echo "Float";
+		} else if(is_bool($item)){
+			echo "Boolean";
+		} else if(is_array($item)){
+			echo "Array";
+		} else if(is_null($item)){
+			echo "Null";
+		} else if(is_string($item)){
+			echo "String";
+		}
+
+		echo PHP_EOL;
+	}
 ?>
