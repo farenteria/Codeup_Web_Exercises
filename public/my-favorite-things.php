a07d125791812652e7f55dcaba9fb81e20919362
Fernando Renteria <rentfern@gmail.com>
2015-06-16 18:33:49 -0500

    PHP favorites - initial echoing of items. These are hardcoded in each row. Will be replaced by a better and dynamic version

--- public/my-favorite-things.php
+++ public/my-favorite-things.php
@@ -7,6 +7,23 @@
 	<title>Favorites</title>
 </head>
 <body>
-	
+	<h1>These are a few of my favorite things</h1>
+	<table>
+<!-- 		<tr>
+			<td><?php echo $favorites[0]; ?></td>
+		</tr>
+		<tr>
+			<td><?php echo $favorites[1]; ?></td>
+		</tr>
+		<tr>
+			<td><?php echo $favorites[2]; ?></td>
+		</tr>
+		<tr>
+			<td><?php echo $favorites[3]; ?></td>
+		</tr>
+		<tr>
+			<td><?php echo $favorites[4]; ?></td>
+		</tr> -->
+	</table>
 </body>
 </html>
