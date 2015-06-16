0872388f150c7d84ee01d33ab61875daddcadd41
Fernando Renteria <rentfern@gmail.com>
2015-06-16 18:14:17 -0500

    PHP server name - Gets a new name with click of button input

--- public/server-name-generator.php
+++ public/server-name-generator.php
@@ -1,6 +1,8 @@
 <?php
 	$adjectives = ["Red", "Small", "Wumbo", "Sliced", "Green", "Mean", "Cracked", "Dipped", "Browned"];
 	$nouns = ["Blurpleberry", "Man", "Cheese", "Tomato", "Plant", "Computer", "Dorito", "Ghost", "Quentin Tarantino", "Movie"];
+	$noun = $nouns[count($nouns) - 1];
+	$adj = $adjectives[count($adjectives) - 1];
 
 	function getNewAdj($adjectives){
 		return $randAdj = $adjectives[rand(0, count($adjectives) - 1)];
@@ -10,7 +12,10 @@
 		return $randNoun = $nouns[rand(0, count($nouns) - 1)];
 	}
 
-	
+	if(isset($_POST["submit"])){
+		$adj =  getNewAdj($adjectives);
+		$noun = getNewNoun($nouns);
+	}
 ?>
 <!DOCTYPE html>
 <html>
@@ -19,7 +24,9 @@
 </head>
 <body>
 	<h1>Your new server name is:</h1>
-	<p><?php echo "$randAdj $randNoun" . PHP_EOL; ?></p>
-	<button id="button" name="button">New Name</button>
+	<p><?php echo "$adj $noun" ?></p>
+	<form method="POST" action="/server-name-generator.php">
+		<input type="submit" name="submit" value="New Name">
+	</form>
 </body>
 </html>
