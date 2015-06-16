594824c3c2343a1db6d1211d6fdb3d4ad392436e
Fernando Renteria <rentfern@gmail.com>
2015-06-16 18:27:58 -0500

    PHP favorites - Added my list of favorite things to do

--- public/my-favorite-things.php
+++ public/my-favorite-things.php
@@ -1,10 +1,12 @@
-<?php  ?>
+<?php
+	$favorites = ["Programming", "Playing guitar", "Playing Smite", "Eating pizza", "Listening to music"];
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Favorites</title>
 </head>
 <body>
-
+	
 </body>
 </html>
