6d670f45ee6d8ad3b81103469475da39bc8f2d74
Fernando Renteria <rentfern@gmail.com>
2015-06-16 17:58:47 -0500

    PHP server name - Moved getting new, random adjective and noun into functions. Added html with a couple of elements

--- public/server-name-generator.php
+++ public/server-name-generator.php
@@ -1,3 +1,25 @@
 <?php
+	$adjectives = ["Red", "Small", "Wumbo", "Sliced", "Green", "Mean", "Cracked", "Dipped", "Browned"];
+	$nouns = ["Blurpleberry", "Man", "Cheese", "Tomato", "Plant", "Computer", "Dorito", "Ghost", "Quentin Tarantino", "Movie"];
 
+	function getNewAdj($adjectives){
+		return $randAdj = $adjectives[rand(0, count($adjectives) - 1)];
+	}
+
+	function getNewNoun($nouns){
+		return $randNoun = $nouns[rand(0, count($nouns) - 1)];
+	}
+
+	
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Your Server Name</title>
+</head>
+<body>
+	<h1>Your new server name is:</h1>
+	<p><?php echo "$randAdj $randNoun" . PHP_EOL; ?></p>
+	<button id="button" name="button">New Name</button>
+</body>
+</html>
