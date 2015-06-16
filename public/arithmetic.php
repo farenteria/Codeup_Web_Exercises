46ff3fe6f0f7ce7127977e6952a89b60ebc96566
Fernando Renteria <rentfern@gmail.com>
2015-06-15 19:34:20 -0500

    PHP arithmetic - added two variables to test scopes

--- public/arithmetic.php
+++ public/arithmetic.php
@@ -1,32 +1,36 @@
 <?php
 
-function add($a, $b)
-{
-    echo $a + $b . PHP_EOL;
-}
+	$a = 9;
+	$b = 10;
+	
+	function add($a, $b)
+	{
+	    echo $a + $b . PHP_EOL;
+	}
 
-function subtract($a, $b)
-{
-    echo $a - $b . PHP_EOL;
-}
+	function subtract($a, $b)
+	{
+	    echo $a - $b . PHP_EOL;
+	}
 
-function multiply($a, $b)
-{
-    echo $a * $b . PHP_EOL;
-}
+	function multiply($a, $b)
+	{
+	    echo $a * $b . PHP_EOL;
+	}
 
-function divide($a, $b)
-{
-    echo $a / $b . PHP_EOL;
-}
+	function divide($a, $b)
+	{
+	    echo $a / $b . PHP_EOL;
+	}
 
-function modulus($a, $b){
-	echo $a % $b . PHP_EOL;
-}
+	function modulus($a, $b){
+		echo $a % $b . PHP_EOL;
+	}
 
-// Add code to test your functions here
-add(5, 5);
-subtract(5, 5);
-multiply(5, 5);
-divide(5, 5);
-modulus(5, 5);
+	// Add code to test your functions here
+	add(5, 5);
+	subtract(5, 5);
+	multiply(5, 5);
+	divide(5, 5);
+	modulus(5, 5);
+?>
