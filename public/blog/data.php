064a1a8e5c51f9ceaed9bb4cee87d9827c6d4138
Fernando Renteria <rentfern@gmail.com>
2015-06-16 21:04:21 -0500

    PHP blog - added data for each post. data.php is now included in index.php

---
+++ public/blog/data.php
@@ -0,0 +1,18 @@
+<?php
+	$author = "Fernando Renteria";
+	$content = "words and words, like so many words";
+	$post = [
+		[
+			"title" => "Happy Post",
+			"date" => "March 13, 2015"
+		],
+		[
+			"title" => "Angry Post",
+			"date" => "March 14, 2015"
+		],
+		[
+			"title" => "Melodramatic Post",
+			"date" => "March 15, 2015"
+		]
+	];
+?>
---
+++ public/blog/index.php
@@ -0,0 +1,18 @@
+<?php
+	include "data.php";
+
+	foreach ($post as $article){
+		$article["author"] = $author;
+		$article["content"] = $content;
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+</body>
+</html>
