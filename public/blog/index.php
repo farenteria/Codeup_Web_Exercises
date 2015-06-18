33bdad728ce1d91e92df94626839be9f1c42988a
Fernando Renteria <rentfern@gmail.com>
2015-06-18 11:52:50 -0500

    PHP blog - include post.php, and added a title to our head

--- public/blog/index.php
+++ public/blog/index.php
@@ -1,18 +1,23 @@
 <?php
 	include "data.php";
 
+	//adds the same author and content to every post
 	for($i = 0; $i < count($post); $i++){
 		$post[$i]["author"] = $author;	
 		$post[$i]["content"] = $content;		
 	}
 
+	//the header of our page
+	include "header.php";
+
+	//each post in our blog
 	include "post.php";
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>The Blog</title>
 </head>
 <body>
 
